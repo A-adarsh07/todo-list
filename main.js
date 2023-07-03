edd3a4e49a8fb47485926d8823dc7e44aaa2e4cb
Adarsh <addy.mishra@gmail.com>
2023-07-03 17:51:43 +0530

    finally Done

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
-const inputBox=document.getElementById("input-box");
-const listContainer=document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+alert("Don't Delay . Start today !!");
 
 function addTask() {
-    if(inputBox.value === ''){
+    if (inputBox.value === '') {
         alert('Please enter a task');
     }
-    else{
+    else {
         let li = document.createElement("li"); // create new element <li>
-        li.innerHTML= inputBox.value;
+        li.innerHTML = inputBox.value;
         listContainer.appendChild(li);  // append the newly created LI to container div
-        let span =document.createElement("span");
-        span.innerHTML= "\u00d7";
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; /*code for cross icon */
         li.appendChild(span);
     }
-    inputBox.value="";
+    inputBox.value = "";
     saveData();
 }
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName ==="LI"){
+// when we click on task it should show checked/unchecked 
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
     }
-    else if(e.target.tagName === "SPAN"){
+    // when we click on cross sign the task should disappear. Here parent element is LI
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
-},false);
+}, false);
 
 // to protect/store the data due to  Page refreshing and  window closed 
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
-function showTask(){
-    listContainer.innerHTML=localStorage.getItem("data");
+// to Display the data whenever we open the app
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
 
